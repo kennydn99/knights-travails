fbbb2745ff80542ab968f13bae6574f029f9a0fd
kennydn99 <73307872+kennydn99@users.noreply.github.com>
2024-08-21 12:52:15 -0700

    Work on knightTravails method

--- knightsTravail.js
+++ knightsTravail.js
@@ -36,7 +36,9 @@ const getPossibleMoves = (square) => {
 
   return possibleMoves;
 };
-console.log(getPossibleMoves([1, 2]));
+console.log("[0, 0] possiblemoves -> ", getPossibleMoves([0, 0]));
+
+console.log("[1, 2] possiblemoves -> ", getPossibleMoves([1, 2]));
 
 const buildAdjacencyList = () => {
   const graph = new AdjacencyList();
@@ -49,23 +51,80 @@ const buildAdjacencyList = () => {
 };
 
 const boardGraph = buildAdjacencyList();
-console.log(boardGraph);
+// console.log(boardGraph);
+
+// const knightTravails = (start, end) => {
+//   const [start_row, start_col] = start;
+//   const [end_row, end_col] = end;
+//   let visited = [];
+//   let queue = [];
+//   queue.push([start_row, start_col]);
+
+//   // while (queue.length > 0) {
+//   const visitedSquare = queue.shift();
+//   console.log("Visitied: ", visitedSquare);
+//   visited.push(visitedSquare);
+//   const [visited_row, visited_col] = visitedSquare;
+//   const nextMoves = boardGraph.getMove(visitedSquare);
+//   nextMoves.forEach((move) => {
+//     const [next_row, next_col] = move;
+//     if (next_row === end_row && next_col === end_col) {
+//       console.log("found end: ", move);
+//       return;
+//     } else {
+//       if (!visited.includes([next_row, next_col])) {
+//         visited.push(move);
+//         queue.push(move);
+//       }
+//     }
+//   });
+
+//   console.log("queue", queue);
+//   // }
+// };
+
+// // knightTravails([0, 0], [1, 2]);
 
 const knightTravails = (start, end) => {
-  const [start_row, start_col] = start;
+  const graph = buildAdjacencyList();
+  let parentMap = {};
+  let visited = [start.toString()];
+  let queue = [[start]];
+
   const [end_row, end_col] = end;
 
-  let queue = [];
-  queue.push([start_row, start_col]);
+  while (queue.length > 0) {
+    const path = queue.shift();
+    const currentSquare = path[path.length - 1];
 
-  // while (queue.length > 0) {
-  const visitedSquare = queue.shift();
-  console.log("Visitied: ", visitedSquare);
-  const nextMoves = boardGraph.getMove(visitedSquare);
-  nextMoves.forEach((move) => queue.push(move));
+    const [curr_row, curr_col] = currentSquare;
 
-  console.log("queue", queue);
-  // }
+    if (curr_row === end_row && curr_col === end_col) {
+      console.log("found end: ", currentSquare);
+      return buildPath(start, end, parentMap);
+    }
+
+    const nextMoves = graph.getMove(currentSquare);
+    nextMoves.forEach((move) => {
+      if (!visited.includes(move.toString())) {
+        visited.push(move.toString());
+        queue.push([...path, move]);
+        parentMap[move.toString()] = currentSquare.toString();
+      }
+    });
+  }
+  return "No Path Found";
 };
 
-knightTravails([0, 0], [1, 2]);
+const buildPath = (start, end, parentMap) => {
+  let path = [];
+
+  while (end.toString() !== start.toString()) {
+    path.push(end);
+    end = parentMap[end.toString()];
+  }
+  path.push(start);
+  return path.reverse();
+};
+
+console.log("Shortest Path", knightTravails([0, 0], [3, 4]));
